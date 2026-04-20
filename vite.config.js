b0911c3ec750f2a5d65881901288d60953ad715f
waruiiko <nerowangx@gmail.com>
2026-04-20 12:55:37 +0930

    fix: use relative base path for Electron file:// compatibility

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   root: path.resolve(__dirname, 'renderer'),
+  base: './',
   plugins: [react()],
   build: {
     outDir: path.resolve(__dirname, 'renderer/dist'),
